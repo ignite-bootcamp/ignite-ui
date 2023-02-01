0b3b8250328230c393e6ae06098256c004c54754
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-01-31 22:00:43 -0300

    adding TextInput component

---
+++ packages/react/src/components/TextInput/styles.ts
@@ -0,0 +1,49 @@
+import { styled } from '../../styles'
+
+export const TextInputContainer = styled('div', {
+  background: '$gray900',
+  padding: '$3 $4',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  border: '2px solid $gray900',
+  display: 'flex',
+  alignItems: 'baseline',
+
+  '&:has(input:focus)': {
+    borderColor: '$ignite300',
+  },
+
+  '&:has(input:disabled)': {
+    cursor: 'not-allowed',
+    opacity: 0.5,
+  },
+})
+
+export const Prefix = styled('span', {
+  fontFamily: '$default',
+  fontSize: '$sm',
+  color: '$gray300',
+  fontWeight: '$regular',
+})
+
+export const Input = styled('input', {
+  fontFamily: '$default',
+  fontSize: '$sm',
+  fontWeight: '$regular',
+  color: '$white',
+  background: 'transparent',
+  width: '100%',
+  border: 0,
+
+  '&:focus': {
+    outline: 0,
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
